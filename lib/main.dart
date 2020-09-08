a1e8fb4d49a76ecb164685b8b7d73c2f0cbb6870
Ru Cindrea <ru.cindrea@altom.com>
2020-09-08 05:31:28 +0300

    Implement Mi Card with personal details

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,78 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.teal,
-        body: Container(),
+        backgroundColor: Colors.grey[850],
+        body: SafeArea(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              CircleAvatar(
+                radius: 80,
+                backgroundImage: AssetImage('images/ru_etna.jpg'),
+              ),
+              Text(
+                'Ru Cindrea',
+                style: TextStyle(
+                    color: Colors.blue,
+                    fontSize: 40.0,
+                    fontWeight: FontWeight.bold,
+                    fontFamily: 'Pacifico'),
+              ),
+              Text(
+                'Flutter Developer and Tester',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              SizedBox(
+                width: 150.0,
+                height: 30.0,
+                child: Divider(
+                  color: Colors.blue[100],
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.phone_iphone, color: Colors.blue),
+                  title: Text(
+                    '+358 458858554',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(Icons.email, color: Colors.blue),
+                  title: Text(
+                    'ru.cindrea@altom.com',
+                    style: TextStyle(
+                      color: Colors.blue,
+                      fontSize: 15.0,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
